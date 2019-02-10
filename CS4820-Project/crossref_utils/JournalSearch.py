8f0f9d86fde6e8a0c370d81fb05e325657500fa1
alex10gauthier <37935779+alex10gauthier@users.noreply.github.com>
2019-02-10 18:25:59 -0400

    Update JournalSearch.py

--- CS4820-Project/crossref_utils/JournalSearch.py
+++ CS4820-Project/crossref_utils/JournalSearch.py
@@ -1,18 +1,24 @@
 #Program that gives sample DOI's from the correct
-#journal for each year from start year to end year
+#journal for each year from start date to end date
 #still a few indescrepencies
 from crossref.restful import Works
 
 
 class JournalSearch:
 
-    def __init__(self, journal_title, journal_package_name, journal_url, journal_publisher, print_issn, online_issn, start_year, end_year):
+    def __init__(self, journal_title, journal_package_name, journal_url, journal_publisher, print_issn,
+                                                        online_issn, start_date, end_date, start_month, end_month,
+                                                        start_year, end_year):
         self.journal_title = journal_title
         self.journal_package_name = journal_package_name
         self.journal_url = journal_url
         self.journal_publisher = journal_publisher
         self.print_issn = print_issn
         self.online_issn = online_issn
+        self.start_date = start_date
+        self.end_date = end_date
+        self.start_month = start_month
+        self.end_month = end_month
         self.start_year = start_year
         self.end_year = end_year
 
@@ -21,28 +27,28 @@ class JournalSearch:
         works = Works()
         doi_list = ['']
 
-        #loop executes until start year reaches the end year also puts results into a list
-        while self.start_year <= self.end_year:
-            print("Journals for year: " + str(self.start_year))
-            for i in works.query(self.journal_title).filter(has_funder='true', has_license='true',
-                                                            issn=self.online_issn,
-                                                            from_pub_date=str(self.start_year) + '-01-01',
-                                                            until_pub_date=str(self.start_year) + '-12-31').sample(10).select('DOI, prefix'):
-                print(str(i))
-                doi_list.append(i)
+        #variable that creates string in correct format for year month and day
+        start_ymd = str(self.start_year) + '-' + str(self.start_month) + '-' + str(self.start_date)
+        end_ymd = str(self.end_year) + '-' + str(self.end_month) + '-' + str(self.end_date)
 
-            self.start_year += 1
+        #loop executes until all the DOI's are put into a list
+        print("Journals for year: " + str(self.start_year))
+        for i in works.query(self.journal_title).filter(has_funder='true', has_license='true',
+                                                            issn=self.online_issn,
+                                                            from_pub_date=start_ymd,
+                                                            until_pub_date=end_ymd).sample(1).select('DOI'):
+            print(str(i['DOI']))
+            doi_list.append(i['DOI'])
 
 
 def main():
     #paramaters have some filler values that aren't needed but there for prooject purposes
     #example used is Journal of Biosciences with correct ISSN
     searcher = JournalSearch('Journal of biosciences', 'Filler-Value', 'filler-value', 'filler-value',
-                             'filler-value', '0973-7138', 2016, 2018)
+                             'filler-value', '0973-7138', '01', 31, '01', 12, 2018, 2018)
     searcher.search_journals()
 
 
 if __name__ == "__main__":
     main()
-
 
