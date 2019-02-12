f339341665966a3a82e7a40488dca7f1d43a9ca7
matt bujold <matt474@live.ca>
2019-02-12 10:22:51 -0400

    Added in a test for valid credentials before trying to send

--- CS4820-Project/email_utils/email_handler.py
+++ CS4820-Project/email_utils/email_handler.py
@@ -5,12 +5,28 @@ from email.message import EmailMessage
 class EmailHandler:
     port = 465
     smtp_server = "smtp.gmail.com"
+    valid_sender = False
 
-    def __init__(self):
+
+    def set_sender(self):
         self.sender = input("Enter the account of the sender: ")
         self.password = input("Please enter a password: ")
+        # try logging in to test
+        try:
+            server = smtplib.SMTP_SSL(self.smtp_server, self.port)
+            server.login(self.sender, self.password)
+            self.valid_sender = True
+        except smtplib.SMTPAuthenticationError:
+            print("The supplied address or password is incorect.")
+            self.valid_sender = False
+
+    def is_valid_sender(self):
+        return self.valid_sender;
 
     def send(self):
+        if not self.valid_sender:
+            print("Can not send an email without a valid sender")
+            return
         receiver = input("Who should I send this message to? ")
         with smtplib.SMTP_SSL(self.smtp_server, self.port) as server:
             server.ehlo()
--- CS4820-Project/email_utils/email_test.py
+++ CS4820-Project/email_utils/email_test.py
@@ -3,4 +3,6 @@ import email_handler
 print("Testing email functionality")
 
 e = email_handler.EmailHandler()
+while not e.is_valid_sender():
+    e.set_sender()
 e.send()
