3deda5724164552a75cecb2842a6360722480ca5
yuuki <yyamada@upei.ca>
2019-02-22 13:54:27 -0400

    made test.py for anyone to edit

--- CS4820-Project/main.py
+++ CS4820-Project/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from time import sleep
 
 import journal_ui.main_ui as main_ui
 import journal_utils.csv_reader as csv_reader
@@ -14,22 +15,13 @@ class MainSystem:
         self.file_path = None
         self.root = tk.Tk()
         self.main_ui = main_ui.MainUI(master=self.root, main_system=self)
-        self.main_ui.mainloop()  # starts UI
+        # self.main_ui.mainloop()  # starts UI
 
     def create_journal_list(self):
         self.journal_list = csv_reader.read_csv_create_journal(self.file_path)
 
-    def test_print(self):
-        n = 0
-        while not (n == -1):
-            n = int(input('Enter an index:'))
-            if n != -1:
-                if self.journal_list is not None:
-                    print(self.journal_list[n])
-                    self.search(self.journal_list[n])
-        print('finished')
-
-    def search(self, journal):
+    @staticmethod
+    def search_article(journal):
         for year in journal.year_dict:
             print(journal.title,
                   journal.year_dict[year][0],  # start_date
@@ -42,50 +34,59 @@ class MainSystem:
             journal.year_dict[year][2].doi = doi
             # journal.year_article_dict[year].doi = doi
             print(doi)
+        print('Search article finished')
 
-    def fetch_article(self, publisher, title, print_issn, online_issn, begin_date, end_date):
-        # return crossref_utils.fetch(publisher, title, print_issn, online_issn, begin_date, end_date);
-        print("crossref.fetch_article is called here")
-
-    def check_reality(self, journal):
-        # return screenscrape_utils.determine_reality(doi)
-        print("journal reality check will be proceeded here")
+    @staticmethod
+    def check_reality(journal):
         scraper = screenscraper.ScreenScraper
         print(journal.title, journal.publisher)
         for year in journal.year_dict:
             # print(journal.year_dict[year][2])
             doi = journal.year_dict[year][2].doi
             print(doi)
-            article = [journal.publisher, doi]
             result = scraper.check_journal(scraper, doi=doi)
             print(str(result))
-
-        # access = screenscrape_utils.determine_reality(doi)
-        # journal.year_dict[year][2].accessible = access
+        print('Reality check finished')
 
     def update(self, code):
         print('CODE:', code)
+
         if code == 'FILE_UPLOADED':
             self.file_path = self.main_ui.input_file_path
             self.create_journal_list()
+            print('SIZE:', len(self.journal_list))
 
         elif code == 'SEARCH_CLICKED':
-            print('SIZE:', len(self.journal_list))
-            self.test_print()
+            n = int(input('Enter an index:'))
+            self.search_article(self.journal_list[n])
 
         elif code == 'DOWNLOAD_CLICKED':
-            print('"Download is ready"')
-            print('enter an index')
             n = int(input('Enter an index:'))
             self.check_reality(self.journal_list[n])
 
 
 def main():
-    main_system = MainSystem()
+    # start()  # the main system
+    test()  # the test system
 
 
 def start():
     main_system = MainSystem()
+
+
+def test():
+    main_system = MainSystem()
+    main_system.file_path = "./journal_utils/journals/use-this.csv"
+    main_system.create_journal_list()
+    n = int(input('Enter an index(-1 to exit):'))
+    while n != -1:
+        journal = main_system.journal_list[n]
+        print('From', journal.expected_subscription_begin,
+              'to', journal.expected_subscription_end,
+              )
+        main_system.search_article(journal)
+        main_system.check_reality(journal)
+        n = int(input('Enter an index(-1 to exit):'))
 
 
 if __name__ == '__main__':
