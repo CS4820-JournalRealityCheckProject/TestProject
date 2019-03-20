69bfe24671e12ec1250a3a45cda28adf48ee6f72
oolatunde <31736377+oolatunde@users.noreply.github.com>
2019-03-20 13:02:59 -0300

    Ui
    changes to UI

--- CS4820-Project/journal_ui/main_ui.py
+++ CS4820-Project/journal_ui/main_ui.py
@@ -78,16 +78,16 @@ class MainUI(tk.Frame):
         self.email_label.grid(row=2, column=1)
 
         # email textfield
-        self.email_textfield = tk.Text(tab1, bd=1, bg='yellow', height=1, width=40)
+        self.email_textfield = tk.Text(tab1, bd=1, bg='grey', height=1, width=40)
         self.email_textfield.grid(row=2, column=2)
 
         # confirm-email label
-        self.confirm_label = tk.Label(tab1, text='Confirm:')
-        self.confirm_label.grid(row=3, column=1)
+        # self.confirm_label = tk.Label(tab1, text='Confirm:')
+        # self.confirm_label.grid(row=3, column=1)
 
         # confirm-email textfield
-        self.confirm_textfield = tk.Text(tab1, bd=1, bg='yellow', height=1, width=40)
-        self.confirm_textfield.grid(row=3, column=2)
+        # self.confirm_textfield = tk.Text(tab1, bd=1, bg='yellow', height=1, width=40)
+        # self.confirm_textfield.grid(row=3, column=2)
 
         # warning message label
         self.warn_var = tk.StringVar()
@@ -151,12 +151,16 @@ class MainUI(tk.Frame):
 
     def start(self):
 
-        if self.confirm_textfield.get('1.0', 'end -1c') != self.email_textfield.get('1.0', 'end -1c') \
-                or self.email_textfield.get('1.0', 'end-1c') == '':
-            print('email did not match')
-            self.warn_var.set('Email is incorrect')
-            return
-        else:
+        # if self.confirm_textfield.get('1.0', 'end -1c') != self.email_textfield.get('1.0', 'end -1c') \
+        #         or self.email_textfield.get('1.0', 'end-1c') == '':
+        #     print('email did not match')
+        #     self.warn_var.set('Email is incorrect')
+        #     return
+        # else:
+        #     self.receiver = self.email_textfield.get('1.0', 'end -1c')
+        #     print(self.receiver)
+
+        if self.email_textfield.get('1.0', 'end -1c') != '':
             self.receiver = self.email_textfield.get('1.0', 'end -1c')
             print(self.receiver)
 
